5999d522163448cfd929548c80f78e0f7eb71f1b
Anderson Dotore <andersondotore@rapidaoapp.com.br>
2021-05-11 00:26:33 -0300

    Projeto em Angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Batman' },
+  { id: 12, name: 'Flash' },
+  { id: 13, name: 'Utópico' },
+  { id: 14, name: 'Magenta' },
+  { id: 15, name: 'Mulher Hulk' },
+  { id: 16, name: 'Darth Vader' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
