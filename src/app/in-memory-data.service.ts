4060a9b4708772d9a46d30a56f518bd67310e2e5
Anderson Dotore <andersondotore@rapidaoapp.com.br>
2021-05-11 11:38:18 -0300

    Finalizando ainda falta o Css

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Batman' },
+      { id: 12, name: 'Flash' },
+      { id: 13, name: 'Utópico' },
+      { id: 14, name: 'Magenta' },
+      { id: 15, name: 'Mulher Hulk' },
+      { id: 16, name: 'Darth Vader' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
+    return {heroes};
+  }
+
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
